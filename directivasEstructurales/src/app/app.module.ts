217f5842a8b9a59c7af8805abe7ba7a4763f8371
florJH <isc.florjardinez@gmail.com>
2022-03-17 09:42:24 -0600

    se agrego componente footer

--- directivasEstructurales/src/app/app.module.ts
+++ directivasEstructurales/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
