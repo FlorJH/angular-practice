28fb59ffbacfd8fe67a8affbe22afd503c70c334
florJH <isc.florjardinez@gmail.com>
2022-03-17 23:19:27 -0600

    se agrego funcion con ngif y ngfor

--- directivasEstructurales/src/app/components/body/body.component.ts
+++ directivasEstructurales/src/app/components/body/body.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
 })
-export class BodyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class BodyComponent {
+  mostrar = true;
+  personajes: string[] =['Spederman', 'Iron-man', 'Hulk'];
+  frase: any = {
+    mensaje: 'Un  gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker',
+  };
 
 }
